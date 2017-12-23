fc1eb0aa66a693800198844fada5801fea22b781
Yongbum Lee <2yongbum@gmail.com>
2017-12-23 15:11:39 +0900

    update sander

--- test/sanding.py
+++ test/sanding.py
@@ -3,41 +3,61 @@ import time
 from threading import Thread
 from optparse import OptionParser
 
-class Sander:
+class Cnc(object):
     def __init__(self, port='COM3', baud=9600):
-        self.ser = serial.Serial(port=port, baudrate=baud,
+        self.port = port
+        self.baud = baud
+        self.ser = None
+        
+    def open(self):
+        self.ser = serial.Serial(port=self.port, baudrate=self.baud,
             parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS, timeout=0)
         time.sleep(1.5)
         if self.ser.isOpen():
             print("COM OPENED")
-            self.ser.write('$x\n'.encode()) 
+            self.ser.write('$x\n'.encode())
+            self.ser.readlines();
         
     def write(self, msg):
         self.ser.write(msg.encode())
         print(">>>"+msg)
         output = self.ser.readline();
-        print("<<<"+output)
+        self.ser.write("?".encode())
+        output = self.ser.readline();
+        print("<<<"+str(output))
     
-    def move(self, x=0, y=0, z=0, sleep=5):
-        if x:
-            msg = 'G0 Z%d\n' % (x)
-            self.write(msg)
-        if y:
-            msg = 'G0 Y%d\n' % (y)
-            self.write(msg)
-        if z:
-            msg = 'G0 X%d\n' % (z)
-            self.write(msg)
-        time.sleep(sleep)
-    
-    def go(self, x=0, y=0, z=0, xstep=10, ystep=100):
-        for v in range(0, y, ystep):
+    def move(self, x=None, y=None, z=None, stay=0):
+        if not self.ser:
+            self.open()
+        print('cnc:move x=%s y=%s z=%s stay=%d' %(x, y, z, stay))
+        msg = 'G0 '
+        if x != None:
+            msg += 'Z%d ' % (x*-1)
+        if y is not None:
+            msg += 'Y%d ' % (y)
+        if z is not None:
+            msg += 'X%d ' % (z)
+        msg += '\n'
+        self.write(msg)
+        time.sleep(stay)
+
+class Sander(Cnc):
+    def __init__(self, opts, args):
+        Cnc.__init__(self)
+        self.stay = 5
+        if args:
+            self.stay = int(args[0])
+            
+    def move(self, x=0, y=0, z=0, xstep=10, ystep=50):
+        print('Sander:move x=%d y=%d z=%d xs=%d ys=%d' %(x, y, z, xstep, ystep))
+        for v in range(0, y+1, ystep):
+            print("v=%d"%v)
+            super().move(y=v)
             for u in range(0, x, xstep):
-                self.move(x=u)
-            for u in range(x, 0-xstep, xstep*-1):
-                self.move(x=u)
-            self.move(y=v)
+                super().move(x=u, stay=self.stay)
+            for u in range(x, 0, xstep*-1):
+                super().move(x=u, stay=self.stay)
 
 def main():
     parser = OptionParser()
@@ -46,12 +66,13 @@ def main():
     parser.add_option('-z', '--z', type='int', dest='z', help='z mm length', default=0)
     parser.add_option('-p', '--port', dest='port', help='serial port', default='COM3')
     parser.add_option('-b', '--baud', type='int', dest='baud', help='baud rate', default=9600)
-    parser.add_option('-s', '--speed', type='int', dest='s', help='speed', default=4)    
+    parser.add_option('-s', '--sand', action='store_true', dest='s', help='sand second')    
     options, args = parser.parse_args()
     
-    cnc = Sander(options.port, options.baud)
-    cnc.go(options.x, options.y, options.z)
-    
+    cnc = Cnc()
+    if options.s:
+        cnc = Sander(options, args)
+    cnc.move(options.x, options.y, options.z)
 
 if __name__ == "__main__":
     main()
